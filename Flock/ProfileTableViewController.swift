e65e1eb1b1c6fa60c0780670bdd7dc23ab1c8f01
Dominic Whyte <Whyte@nat-oitwireless-inside-vapornet100-d-23871.Princeton.EDU>
2017-02-21 22:08:50 -0500

    profile

---
+++ Flock/ProfileTableViewController.swift
@@ -0,0 +1,203 @@
+//
+//  ProfileTableViewController.swift
+//  Flock
+//
+//  Created by Dominic Whyte on 21/02/17.
+//  Copyright © 2017 Dominic Whyte. All rights reserved.
+//
+
+import UIKit
+
+class ProfileTableViewController: UITableViewController {
+    
+    struct Constants {
+        static let SECTION_TITLES = ["Plans"]
+        static let CELL_HEIGHT = 75
+    }
+    
+    var delegate : ProfileDelegate?
+    var plans: [Plan] = [Plan]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.tableView.scrollIndicatorInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        tableView.register(UINib(nibName: "VenueFriendTableViewCell", bundle: nil), forCellReuseIdentifier: "VENUE_FRIEND")
+        
+    }
+    
+    
+    // TABLEVIEW FUNCTIONS
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let returnedView = UIView(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 25))
+        //returnedView.backgroundColor = FlockColors.FLOCK_BLUE
+        
+        let gradient = CAGradientLayer()
+        
+        gradient.frame = returnedView.bounds
+        
+        gradient.startPoint = CGPoint(x: 0, y: 0.5)
+        gradient.endPoint = CGPoint(x: 1, y: 0.5)
+        gradient.colors = [FlockColors.FLOCK_BLUE.cgColor, FlockColors.FLOCK_LIGHT_BLUE.cgColor]
+        
+        returnedView.layer.insertSublayer(gradient, at: 0)
+        
+        let label = UILabel(frame: CGRect(x: 10, y: 0, width: view.frame.size.width, height: 25))
+        label.textColor = .white
+        label.text = Constants.SECTION_TITLES[section]
+        returnedView.addSubview(label)
+        
+        return returnedView
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return Constants.SECTION_TITLES[section]
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return Constants.SECTION_TITLES.count
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.plans.count
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let plan = plans[indexPath.row]
+        Utilities.animateToPlacesTabWithVenueIDandDate(venueID: plan.venueID, date: plan.date)
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let plan = self.plans[indexPath.row]
+        let venue = appDelegate.venues[plan.venueID]!
+        let cell = tableView.dequeueReusableCell(withIdentifier: "VENUE_FRIEND", for: indexPath) as! VenueFriendTableViewCell
+        cell.nameLabel.text = venue.VenueName
+        cell.subtitleLabel.text = DateUtilities.convertDateToStringByFormat(date: plan.date, dateFormat: "MMMM d")
+        if let venueImage = appDelegate.venueImages[venue.ImageURL] {
+            cell.profilePic.image = venueImage
+            cell.profilePic.clipsToBounds = true
+            cell.profilePic.layer.borderWidth = 2
+            cell.profilePic.layer.borderColor = UIColor.lightGray.cgColor
+        }
+        else {
+            appDelegate.getMissingImage(imageURL: venue.ImageURL, completion: { (status) in
+                if (status) {
+                    DispatchQueue.main.async {
+                        if let venueImage = appDelegate.venueImages[venue.ImageURL] {
+                            cell.profilePic.image = venueImage
+                            cell.profilePic.clipsToBounds = true
+                            cell.profilePic.layer.borderWidth = 2
+                            cell.profilePic.layer.borderColor = UIColor.lightGray.cgColor
+                        }
+                        else {
+                            Utilities.printDebugMessage("Error: could not retrieve image")
+                        }
+                    }
+                }
+            })
+        }
+        cell.selectionStyle = .none
+        return cell
+    }
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return CGFloat(Constants.CELL_HEIGHT)
+    }
+    
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if (editingStyle == UITableViewCellEditingStyle.delete) {
+            // handle delete (by removing the data from your array and updating the tableview)
+        }
+    }
+    
+    //UpdateTableViewDelegate function
+    func updateDataAndTableView(_ completion: @escaping (Bool) -> Void) {
+        let loadingScreen = Utilities.presentLoadingScreen(vcView: self.view)
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        appDelegate.updateAllData { (success) in
+            DispatchQueue.main.async {
+                Utilities.removeLoadingScreen(loadingScreenObject: loadingScreen, vcView: self.view)
+                if (success) {
+                    if let delegate = self.delegate {
+                        delegate.setupUser()
+                    }
+                    else {
+                        Utilities.printDebugMessage("Error with delegate in profile")
+                    }
+                    if let tableView = self.tableView {
+                        tableView.reloadData()
+                    }
+                    else {
+                        Utilities.printDebugMessage("Error 2: with reloading table view")
+                    }
+                }
+                else {
+                    Utilities.printDebugMessage("Error updating and reloading data in table view")
+                }
+                completion(success)
+            }
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let plan = self.plans[indexPath.row]
+        let venue = appDelegate.venues[plan.venueID]!
+        let date = DateUtilities.getStringFromDate(date: plan.date)
+        
+        let delete = UITableViewRowAction(style: .destructive, title: "Delete") { (action, indexPath) in
+            // delete item at indexPath
+            let loadingScreen = Utilities.presentLoadingScreen(vcView: self.view)
+            FirebaseClient.addUserToVenuePlansForDate(date: date, venueID: venue.VenueID, userID: appDelegate.user!.FBID, add: false, completion: { (success) in
+                if (success) {
+                    Utilities.printDebugMessage("Successfully removed plan to attend venue")
+                    self.updateDataAndTableView({ (success) in
+                        Utilities.removeLoadingScreen(loadingScreenObject: loadingScreen, vcView: self.view)
+                        if (success) {
+                            DispatchQueue.main.async {
+                                if let delegate = self.delegate  {
+                                    delegate.displayUnAttendedPopup(venueName: venue.VenueNickName, attendFullDate: date)
+                                }
+                                else {
+                                    Utilities.printDebugMessage("Error with delegate in profile")
+                                }
+                            }
+                        }
+                        else {
+                            Utilities.printDebugMessage("Error reloading tableview in venues")
+                        }
+                    })
+                }
+                else {
+                    Utilities.printDebugMessage("Error adding user to venue plans for date")
+                    Utilities.removeLoadingScreen(loadingScreenObject: loadingScreen, vcView: self.view)
+                }
+            })
+        }
+        
+        /* V2.0
+         let share = UITableViewRowAction(style: .normal, title: "Share") { (action, indexPath) in
+         // share item at indexPath
+         }
+         */
+        
+        //share.backgroundColor = FlockColors.FLOCK_BLUE
+        delete.backgroundColor = FlockColors.FLOCK_GRAY
+        
+        //return [delete, share]
+        return [delete]
+    }
+
+}
+
+protocol ProfileDelegate: class {
+    func displayUnAttendedPopup(venueName : String, attendFullDate : String)
+    func setupUser()
+}
--- Flock/ProfileViewController.swift
+++ Flock/ProfileViewController.swift
@@ -13,12 +13,8 @@ import SCLAlertView
 import FacebookShare
 import FBSDKShareKit
 
-class ProfileViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    
-    struct Constants {
-        static let SECTION_TITLES = ["Plans"]
-        static let CELL_HEIGHT = 75
-    }
+class ProfileViewController: UIViewController, ProfileDelegate {
+   
     
     // Variables other ViewControllers might be concerned with
     var user: User?
@@ -26,22 +22,36 @@ class ProfileViewController: UIViewController, UITableViewDelegate, UITableViewD
     var plans: [Plan] = [Plan]()
     let multiPscope = PermissionScope()
  
-    @IBOutlet weak var tableView: UITableView!
+    var tableView: UITableView?
+    var tableViewController : ProfileTableViewController?
+    
     override func viewDidLoad() {
         
         super.viewDidLoad()
         self.setupUser()
-        self.tableView.delegate = self
-        self.tableView.dataSource = self
-        self.tableView.scrollIndicatorInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
-        tableView.register(UINib(nibName: "VenueFriendTableViewCell", bundle: nil), forCellReuseIdentifier: "VENUE_FRIEND")
+        
 
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        if(appDelegate.profileNeedsToUpdate) {
+            Utilities.printDebugMessage("Updating profile page")
+            self.setupUser()
+            appDelegate.profileNeedsToUpdate = true
+            self.view.setNeedsLayout()
+            self.view.setNeedsDisplay()
+            if let tableView = self.tableView {
+                tableView.reloadData()
+            }
+            else {
+                Utilities.printDebugMessage("Error reloading tableview from parent")
+            }
+            
+        }
     }
     
     func setupUser() {
@@ -59,14 +69,23 @@ class ProfileViewController: UIViewController, UITableViewDelegate, UITableViewD
         
         self.user = user!
         //self.username = user!.Name
-        self.plans = Array(user!.Plans.values).filter({ (plan) -> Bool in
-            Utilities.printDebugMessage("VenueID: \(plan.venueID), Date: \(plan.date)")
-            return DateUtilities.isValidTimeFrame(dayDiff: DateUtilities.daysUntilPlan(planDate: plan.date))
-            
-        })
+        
+        if let tableVC = self.tableViewController {
+            tableVC.plans = Array(user!.Plans.values).filter({ (plan) -> Bool in
+                Utilities.printDebugMessage("VenueID: \(plan.venueID), Date: \(plan.date)")
+                return DateUtilities.isValidTimeFrame(dayDiff: DateUtilities.daysUntilPlan(planDate: plan.date))
+                
+            })
+
+        }
+        else {
+            Utilities.printDebugMessage("Error 3: reloading tableVC from parent")
+        }
+        
         FirebaseClient.getImageFromURL(user!.PictureURL) { (image) in
             DispatchQueue.main.async {
                 //self.profileImage = image
+                Utilities.printDebugMessage("this aint doing shit")
             }
         }
     }
@@ -95,160 +114,10 @@ class ProfileViewController: UIViewController, UITableViewDelegate, UITableViewD
         self.present(optionMenu, animated: true, completion: nil)
     }
 
-    // TABLEVIEW FUNCTIONS
-    
-    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        let returnedView = UIView(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 25))
-        //returnedView.backgroundColor = FlockColors.FLOCK_BLUE
-        
-        let gradient = CAGradientLayer()
-        
-        gradient.frame = returnedView.bounds
-        
-        gradient.startPoint = CGPoint(x: 0, y: 0.5)
-        gradient.endPoint = CGPoint(x: 1, y: 0.5)
-        gradient.colors = [FlockColors.FLOCK_BLUE.cgColor, FlockColors.FLOCK_LIGHT_BLUE.cgColor]
-        
-        returnedView.layer.insertSublayer(gradient, at: 0)
-        
-        let label = UILabel(frame: CGRect(x: 10, y: 0, width: view.frame.size.width, height: 25))
-        label.textColor = .white
-        label.text = Constants.SECTION_TITLES[section]
-        returnedView.addSubview(label)
-        
-        return returnedView
-    }
-    
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return Constants.SECTION_TITLES[section]
-    }
-    
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return Constants.SECTION_TITLES.count
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.plans.count
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let plan = plans[indexPath.row]
-        Utilities.animateToPlacesTabWithVenueIDandDate(venueID: plan.venueID, date: plan.date)
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        let appDelegate = UIApplication.shared.delegate as! AppDelegate
-        let plan = self.plans[indexPath.row]
-        let venue = appDelegate.venues[plan.venueID]!
-        let cell = tableView.dequeueReusableCell(withIdentifier: "VENUE_FRIEND", for: indexPath) as! VenueFriendTableViewCell
-        cell.nameLabel.text = venue.VenueName
-        cell.subtitleLabel.text = DateUtilities.convertDateToStringByFormat(date: plan.date, dateFormat: "MMMM d")
-        if let venueImage = appDelegate.venueImages[venue.ImageURL] {
-            cell.profilePic.image = venueImage
-            cell.profilePic.clipsToBounds = true
-            cell.profilePic.layer.borderWidth = 2
-            cell.profilePic.layer.borderColor = UIColor.lightGray.cgColor
-        }
-        else {
-            appDelegate.getMissingImage(imageURL: venue.ImageURL, completion: { (status) in
-                if (status) {
-                    DispatchQueue.main.async {
-                        if let venueImage = appDelegate.venueImages[venue.ImageURL] {
-                            cell.profilePic.image = venueImage
-                            cell.profilePic.clipsToBounds = true
-                            cell.profilePic.layer.borderWidth = 2
-                            cell.profilePic.layer.borderColor = UIColor.lightGray.cgColor
-                        }
-                        else {
-                            Utilities.printDebugMessage("Error: could not retrieve image")
-                        }
-                    }
-                }
-            })
-        }
-        cell.selectionStyle = .none
-        return cell
-    }
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return CGFloat(Constants.CELL_HEIGHT)
-    }
-    
-    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    
-    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if (editingStyle == UITableViewCellEditingStyle.delete) {
-            // handle delete (by removing the data from your array and updating the tableview)
-        }
-    }
-    
-    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
-        
-        let appDelegate = UIApplication.shared.delegate as! AppDelegate
-        let plan = self.plans[indexPath.row]
-        let venue = appDelegate.venues[plan.venueID]!
-        let date = DateUtilities.getStringFromDate(date: plan.date)
-        
-        let delete = UITableViewRowAction(style: .destructive, title: "Delete") { (action, indexPath) in
-            // delete item at indexPath
-            let loadingScreen = Utilities.presentLoadingScreen(vcView: self.view)
-            FirebaseClient.addUserToVenuePlansForDate(date: date, venueID: venue.VenueID, userID: appDelegate.user!.FBID, add: false, completion: { (success) in
-                if (success) {
-                    Utilities.printDebugMessage("Successfully removed plan to attend venue")
-                    self.updateDataAndTableView({ (success) in
-                        Utilities.removeLoadingScreen(loadingScreenObject: loadingScreen, vcView: self.view)
-                        if (success) {
-                            DispatchQueue.main.async {
-                                self.displayUnAttendedPopup(venueName: venue.VenueNickName, attendFullDate: date)
-                            }
-                        }
-                        else {
-                            Utilities.printDebugMessage("Error reloading tableview in venues")
-                        }
-                    })
-                }
-                else {
-                    Utilities.printDebugMessage("Error adding user to venue plans for date")
-                    Utilities.removeLoadingScreen(loadingScreenObject: loadingScreen, vcView: self.view)
-                }
-            })
-        }
-        
-        /* V2.0
-         let share = UITableViewRowAction(style: .normal, title: "Share") { (action, indexPath) in
-         // share item at indexPath
-         }
-         */
-        
-        //share.backgroundColor = FlockColors.FLOCK_BLUE
-        delete.backgroundColor = FlockColors.FLOCK_GRAY
-        
-        //return [delete, share]
-        return [delete]
-    }
 
     // END OF TABLEVIEW FUNCTIONS
     
-    //UpdateTableViewDelegate function
-    func updateDataAndTableView(_ completion: @escaping (Bool) -> Void) {
-        let loadingScreen = Utilities.presentLoadingScreen(vcView: self.view)
-        let appDelegate = UIApplication.shared.delegate as! AppDelegate
-        appDelegate.updateAllData { (success) in
-            DispatchQueue.main.async {
-                Utilities.removeLoadingScreen(loadingScreenObject: loadingScreen, vcView: self.view)
-                if (success) {
-                    self.setupUser()
-                    self.tableView.reloadData()
-                }
-                else {
-                    Utilities.printDebugMessage("Error updating and reloading data in table view")
-                }
-                completion(success)
-            }
-        }
-    }
+    
     
     func displayUnAttendedPopup(venueName : String, attendFullDate : String) {
         let displayDate = DateUtilities.convertDateToStringByFormat(date: DateUtilities.getDateFromString(date: attendFullDate), dateFormat: DateUtilities.Constants.uiDisplayFormat)
@@ -260,14 +129,16 @@ class ProfileViewController: UIViewController, UITableViewDelegate, UITableViewD
     
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        if let tableViewController = segue.destination as? ProfileTableViewController {
+            self.tableView = tableViewController.tableView
+            self.tableViewController = tableViewController
+            tableViewController.delegate = self
+        }
+        else {
+            Utilities.printDebugMessage("Error: could not get table vc")
+        }
     }
-    */
+    
 
 }
